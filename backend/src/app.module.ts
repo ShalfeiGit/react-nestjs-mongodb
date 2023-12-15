c0db60cbe00d9035b41fc6019cb4309bab8849cc
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2023-12-15 10:14:07 +0700

    middle changes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ArticleModule } from './article/article.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../frontend/dist'),
     }),
+    ArticleModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
