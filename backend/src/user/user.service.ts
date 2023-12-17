79892468490834a395c0f472baa6a86044f6b5c4
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2023-12-17 16:41:22 +0700

    fix user crud

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,5 +1,5 @@
 ﻿import { Injectable } from '@nestjs/common';
-import { DeleteResult, Repository } from 'typeorm';
+import { DeleteResult, InsertResult, Repository, UpdateResult } from 'typeorm';
 import { User } from './user.entity';
 import { createUserDto } from './dtos/createUserDto';
 import { updateUserDto } from './dtos/updateUserDto';
@@ -13,28 +13,40 @@ export class UserService {
   ) {}
 
   async getUser(username: string): Promise<User> {
-    return await this.userRepository.findOne({ where: { username } });
+    return await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.username = :username', { username })
+      .getOne();
   }
 
-  async saveUser(dto: createUserDto): Promise<User> {
-    return await this.userRepository.save({ ...dto });
+  async saveUser(dto: createUserDto): Promise<InsertResult> {
+    return await this.userRepository
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values([{ ...dto }])
+      .execute();
   }
 
-  async updateUser(username: string, dto: updateUserDto): Promise<User> {
-    const property = await this.userRepository.findOne({
-      where: { username },
-    });
-
-    return this.userRepository.save({
-      ...property,
-      ...dto,
-    });
+  async updateUser(
+    username: string,
+    dto: updateUserDto,
+  ): Promise<UpdateResult> {
+    return this.userRepository
+      .createQueryBuilder()
+      .update(User)
+      .set({ ...dto })
+      .where('username = :username', { username })
+      .execute();
   }
 
-  async deleteUser(dto: createUserDto): Promise<DeleteResult> {
-    const deleteResult = await this.userRepository.delete({
-      username: dto.username,
-    });
+  async deleteUser(username: string): Promise<DeleteResult> {
+    const deleteResult = await this.userRepository
+      .createQueryBuilder()
+      .delete()
+      .from(User)
+      .where('username = :username', { username })
+      .execute();
     return deleteResult;
   }
 }
