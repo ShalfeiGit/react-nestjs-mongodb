307d84066095f2fee7d9ca440e6e12c8f1cae74c
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2023-12-18 22:36:46 +0700

    add config

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -7,7 +7,7 @@ export class AuthService {
     const payloadTokenRefresh = { password, username };
     return {
       access_token: sign(payloadToken, process.env.JWT_SECRET_SALT),
-      refresh_token: sign(payloadTokenRefresh, 'Custom_secret_value'),
+      refresh_token: sign(payloadTokenRefresh, process.env.JWT_SECRET_SALT),
     };
   }
 }
