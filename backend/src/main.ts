c37ffba642c78a2db80a1e88bc60111a9a907c34
Valentin Zadorozhniy <Zadorozhnaya@mail.ru>
2024-02-11 19:40:34 +0700

    add protection

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
-import { AuthGuard } from './auth/auth.guard';
+import * as csurf from 'csurf';
+import helmet from 'helmet';
+import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
@@ -10,6 +12,14 @@ async function bootstrap() {
     credentials: true,
   });
   app.use(cookieParser('Custom secret value'));
+  app.use(csurf({ cookie: { sameSite: true } }));
+  app.use((req: any, res: any, next: any) => {
+    const token = req.csrfToken();
+    res.cookie('XSRF-TOKEN', token);
+    res.locals.csrfToken = token;
+    next();
+  });
+  app.use(helmet());
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
